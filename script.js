9f93dc5e98ac57c6d62d68958762b9c6c008e27d
Victor <147210005+VictorSerur@users.noreply.github.com>
2023-10-06 16:26:40 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// When clicking a navigation link, smoothly scroll to the specific section
+const linksElements = document.querySelectorAll('.links a')
+linksElements.forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault()
+        const sectionID = link.getAttribute('href')
+        const sectionElement = document.querySelector(sectionID)
+        // Subtract 50 pixels to account for the fixed header height
+        const offsetTop = sectionElement.offsetTop - 50
+        scroll({
+            top: offsetTop,
+            behavior: 'smooth'
+
+        })
+    })
+})
+
+$('.about img').fadeOut(0).fadeIn(500)
+$('.about p').fadeOut(0).slideDown(1000)
